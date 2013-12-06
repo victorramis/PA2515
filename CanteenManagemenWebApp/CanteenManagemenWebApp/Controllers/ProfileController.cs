c8958a52ff1be303dc5864f6063b397ce4fd7a77
zakapong <zaka.pong@yahoo.com>
2013-12-06 11:46:27 +0100

    User detail profile page

---
+++ CanteenManagemenWebApp/CanteenManagemenWebApp/Controllers/ProfileController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CanteenManagemenWebApp.Models;
+
+namespace CanteenManagemenWebApp.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private CanteenContext db = new CanteenContext();
+
+        //
+        // GET: /Profile/
+
+        public ActionResult Index()
+        {
+            return View(db.UserProfiles.ToList());
+        }
+
+        //
+        // GET: /Profile/Details/5
+
+        public ActionResult Details(int id)
+        {
+            UserProfile userprofile = db.UserProfiles.Find(id);
+            if (userprofile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userprofile);
+        }
+        public ActionResult MyDetails()
+        {
+
+            var userprofile = new UserProfile();
+            using (CanteenContext ctx = new CanteenContext())
+            {
+                userprofile = (from o in ctx.UserProfiles orderby o.UserId where o.UserName == User.Identity.Name select o).ToList().FirstOrDefault();
+            }
+            if (userprofile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userprofile);
+        }
+
+        //
+        // GET: /Profile/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Profile/Create
+
+        [HttpPost]
+        public ActionResult Create(UserProfile userprofile)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserProfiles.Add(userprofile);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(userprofile);
+        }
+
+        //
+        // GET: /Profile/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            UserProfile userprofile = db.UserProfiles.Find(id);
+            if (userprofile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userprofile);
+        }
+
+        //
+        // POST: /Profile/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(UserProfile userprofile)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userprofile).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("MyDetails");
+            }
+            return View(userprofile);
+        }
+
+        //
+        // GET: /Profile/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            UserProfile userprofile = db.UserProfiles.Find(id);
+            if (userprofile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userprofile);
+        }
+
+        //
+        // POST: /Profile/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserProfile userprofile = db.UserProfiles.Find(id);
+            db.UserProfiles.Remove(userprofile);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
